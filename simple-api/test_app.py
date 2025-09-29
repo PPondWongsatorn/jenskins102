0b6885446b9ef706137083f2cd1261ecc67cd9a7
nittozz <patiphanpk7@gmail.com>
2025-09-29 14:04:53 +0700

    api_unitest4

--- simple-api/test_app.py
+++ simple-api/test_app.py
@@ -8,19 +8,19 @@ class TestAPI(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def true_when_x_is_17(self):
+    def test_true_when_x_is_17(self):
         response = self.app.get('/is_prime/17')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertEqual(data['output'], 'true')
 
-    def false_when_x_is_36(self):
+    def test_false_when_x_is_36(self):
         response = self.app.get('/is_prime/36')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertEqual(data['output'], 'false')
     
-    def true_when_x_is_13219(self):
+    def test_true_when_x_is_13219(self):
         response = self.app.get('/is_prime/13219')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
